e10e32ad6c231359bdbf6dbd37eb199e3e664457
Srinivas Coimbatore <cjsrinivas@Srinivass-MacBook-Pro.local>
2016-04-16 14:42:30 -0400

    modified:   recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# Create the webusrs
+group 'webusrs' do
+  action :create
+end
+
+# Create the coimbato user
+user 'coimbato' do
+  comment 'Srinivas Coimbatore'
+  gid 'webusrs'
+  home '/home/coimbato'
+  shell '/bin/bash'
+end
+
+# Install apache2
+package 'apache2'
+
+# Start apache2 and make sure that the
+# service will start after a reboot
+service 'apache2' do
+  action [:start, :enable]
+end
